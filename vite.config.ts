10dbed24c14266ba700beca31c9af738ca6b2724
fox-sd1 <mhmdmhmoud077@gmail.com>
2026-04-20 23:07:24 +0200

    Configure base path for offline support
    Set base path to './' for offline application support.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // <--- هذا هو السطر السحري ليعمل التطبيق أوفلاين
   server: {
     host: "::",
     port: 8080,
